7b067bf1ca72464f3a4dce6fceaad94a91bebdea
Ammar Hammoud <st.ammar.hammoud@gmail.com>
2025-07-20 12:38:22 +0300

    changed the design of sign up screen

--- lib/pages/sign_up_screen/sign_up.dart
+++ lib/pages/sign_up_screen/sign_up.dart
@@ -10,7 +10,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 
-
+import '../../shared/constants/constants.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -18,147 +18,189 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create:(context) => SignUpScreenCubit(),
-      child: BlocConsumer<SignUpScreenCubit,SignUpScreenStates>(
-        listener:(context,state){},
-        builder:(context, state){
+      create: (context) => SignUpScreenCubit(),
+      child: BlocConsumer<SignUpScreenCubit, SignUpScreenStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
           var signUpCubitObject = SignUpScreenCubit.get(context);
+          var screenHeight = MediaQuery.of(context).size.height;
           return Scaffold(
-              body: Container(
-                padding:EdgeInsets.all(30),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                   Text( 'Create an\naccont ',style: TextStyle( fontSize:40 ,color: Colors.black,fontWeight: FontWeight.bold)),
-                    SizedBox(height: 30,),
-                    ValidatedTextField(
-                      controller:
-                      signUpCubitObject.userTextController.nameController,
-                      validator:
-                      signUpCubitObject.userTextValidators.nameValidator,
-                      errorText: 'name field cannot be empty',
-                      hintText: 'name',
-                      icon: Icons.person,
-                    ),
-                    ValidatedTextField(
-                      controller:
-                      signUpCubitObject.userTextController.emailController,
-                      validator:
-                      signUpCubitObject.userTextValidators.emailValidator,
-                      errorText: 'email field cannot be empty',
-                      hintText: 'email',
-                      icon: Icons.email,
-                    ),
-                    SizedBox(height: 20,),
-                    ValidatedTextField(
-                     icon: Icons.phone,
-                      controller:
-                      signUpCubitObject.userTextController.phoneController,
-                      validator:
-                      signUpCubitObject.userTextValidators.phoneValidator,
-                      errorText: 'phone field cannot be empty',
-                      hintText: 'phone',
-                    ),
-                    SizedBox(height: 20,),
-                    ValidatedTextField(
-                      icon: Icons.lock,
-                      obscureText: true,
-                      controller:
-                      signUpCubitObject.userTextController.passwordController,
-                      validator:
-                      signUpCubitObject.userTextValidators.passwordValidator,
-                      errorText: 'password field cannot be empty',
-                      hintText: 'password',
-                    ),
-                    SizedBox(height: 30,),
-                    CustomizedButton(
-                      title: 'SignUp',
-                        condition: state is! SignUpScreenLoadingState,
-                        onPressed: () {
-                          signUpCubitObject.signUp(context: context);
-                        }
-                    ),
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Center(
-                        child: Text(
-                          "- OR Continue With -",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                              color: Color(0xff575757)),
-                        )),
-                    SizedBox(height: 20,),
-                    // Row(
-                    //   mainAxisAlignment: MainAxisAlignment.center,
-                    //   crossAxisAlignment: CrossAxisAlignment.center,
-                    //   children: [
-                    //     CircleImageWithBorder(
-                    //       borderColor: AppColors.primary,
-                    //       borderWidth: 1,
-                    //       imagePath: NetworkImage('assets/images/apple 1.png'),
-                    //       size: 56,
-                    //     ),
-                    //     SizedBox(width: 20,),
-                    //     CircleImageWithBorder(
-                    //       borderColor: AppColors.primary,
-                    //       borderWidth: 1,
-                    //       imagePath: NetworkImage('assets/images/facebook-app-symbol 1.png'),
-                    //       size: 56,
-                    //     ),
-                    //     SizedBox(width: 20,),
-                    //     CircleImageWithBorder(
-                    //       borderColor: AppColors.primary,
-                    //       borderWidth: 1,
-                    //       imagePath: NetworkImage('assets/images/Frame 4.png') ,
-                    //       size: 56,
-                    //     ),
-                    //     SizedBox(height: 30,),
-                    //
-                    //   ],
-                    // ),
-                    SizedBox(height: 30,),
-                    Row(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  color: Constants.primaryColor,
+                  height: screenHeight * 0.3,
+                  width: double.infinity,
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          width: 115,
+                        Expanded(child: SizedBox()),
+                        Text(
+                          'Go ahead and set up\nyour account',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(color: Colors.white),
                         ),
-                        // Text(
-                        //   "I Already Have an Account ?",
-                        //   style: TextStyle(color: Color(0xff575757)),
-                        // ),
-                        // SizedBox(
-                        //   width: 4,
-                        // ),
-                        // TextButton(
-                        //     onPressed: () {
-                        //       Navigator.push(
-                        //         context,
-                        //         MaterialPageRoute(
-                        //             builder: (context) =>
-                        //                 LoginScreen()), // استبدل SecondPage بالصفحة التي ترغب في الانتقال إليها
-                        //       );
-                        //     },
-                        //     child: Text(
-                        //       "Login",
-                        //       style: TextStyle(
-                        //           color: Color(0xffF83758),
-                        //           fontWeight: FontWeight.bold),
-                        //     )),
+                        Text(
+                          'Sign in-up enjoy the best experience',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleSmall!
+                              .copyWith(color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
                       ],
-                    )
-
-
-                  ],
+                    ),
+                  ),
                 ),
-              ),
-
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    transform: Matrix4.translationValues(0.0, -25.0, 0.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: SingleChildScrollView(
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                              maxHeight: MediaQuery
+                                  .of(context)
+                                  .size
+                                  .height * 0.45,),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ValidatedTextField(
+                                controller: signUpCubitObject
+                                    .userTextController.nameController,
+                                validator: signUpCubitObject
+                                    .userTextValidators.nameValidator,
+                                errorText: 'name field cannot be empty',
+                                hintText: 'name',
+                                icon: Icons.person,
+                              ),
+                              ValidatedTextField(
+                                controller: signUpCubitObject
+                                    .userTextController.emailController,
+                                validator: signUpCubitObject
+                                    .userTextValidators.emailValidator,
+                                errorText: 'email field cannot be empty',
+                                hintText: 'email',
+                                icon: Icons.email,
+                              ),
+                              ValidatedTextField(
+                                icon: Icons.phone,
+                                controller: signUpCubitObject
+                                    .userTextController.phoneController,
+                                validator: signUpCubitObject
+                                    .userTextValidators.phoneValidator,
+                                errorText: 'phone field cannot be empty',
+                                hintText: 'phone',
+                              ),
+                              ValidatedTextField(
+                                icon: Icons.lock,
+                                obscureText: true,
+                                controller: signUpCubitObject
+                                    .userTextController.passwordController,
+                                validator: signUpCubitObject
+                                    .userTextValidators.passwordValidator,
+                                errorText: 'password field cannot be empty',
+                                hintText: 'password',
+                              ),
+                              CustomizedButton(
+                                  title: 'SignUp',
+                                  condition: state is! SignUpScreenLoadingState,
+                                  onPressed: () {
+                                    signUpCubitObject.signUp(context: context);
+                                  }),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                // Center(
+                //   child: Text(
+                //     "- OR Continue With -",
+                //     style: TextStyle(
+                //         fontWeight: FontWeight.bold,
+                //         fontSize: 15,
+                //         color: Color(0xff575757)),
+                //   ),
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.center,
+                //   crossAxisAlignment: CrossAxisAlignment.center,
+                //   children: [
+                //     CircleImageWithBorder(
+                //       borderColor: AppColors.primary,
+                //       borderWidth: 1,
+                //       imagePath: NetworkImage('assets/images/apple 1.png'),
+                //       size: 56,
+                //     ),
+                //     SizedBox(width: 20,),
+                //     CircleImageWithBorder(
+                //       borderColor: AppColors.primary,
+                //       borderWidth: 1,
+                //       imagePath: NetworkImage('assets/images/facebook-app-symbol 1.png'),
+                //       size: 56,
+                //     ),
+                //     SizedBox(width: 20,),
+                //     CircleImageWithBorder(
+                //       borderColor: AppColors.primary,
+                //       borderWidth: 1,
+                //       imagePath: NetworkImage('assets/images/Frame 4.png') ,
+                //       size: 56,
+                //     ),
+                //     SizedBox(height: 30,),
+                //
+                //   ],
+                // ),
+                // Row(
+                //   children: [
+                //     SizedBox(
+                //       width: 115,
+                //     ),
+                //     // Text(
+                //     //   "I Already Have an Account ?",
+                //     //   style: TextStyle(color: Color(0xff575757)),
+                //     // ),
+                //     // SizedBox(
+                //     //   width: 4,
+                //     // ),
+                //     // TextButton(
+                //     //     onPressed: () {
+                //     //       Navigator.push(
+                //     //         context,
+                //     //         MaterialPageRoute(
+                //     //             builder: (context) =>
+                //     //                 LoginScreen()), // استبدل SecondPage بالصفحة التي ترغب في الانتقال إليها
+                //     //       );
+                //     //     },
+                //     //     child: Text(
+                //     //       "Login",
+                //     //       style: TextStyle(
+                //     //           color: Color(0xffF83758),
+                //     //           fontWeight: FontWeight.bold),
+                //     //     )),
+                //   ],
+                // )
+              ],
+            ),
           );
-
-        } ,
+        },
       ),
-    ) ;
+    );
   }
 }
